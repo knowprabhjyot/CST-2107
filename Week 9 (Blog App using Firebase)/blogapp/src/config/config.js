8415629824f6ba2a33e1c00b29e4590c1a5af09d
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2023-07-07 16:44:21 -0700

    blog creation using firebase

--- Week 9 (Blog App using Firebase)/blogapp/src/config/config.js
+++ Week 9 (Blog App using Firebase)/blogapp/src/config/config.js
@@ -4,13 +4,6 @@ import { FacebookAuthProvider, getAuth, GoogleAuthProvider } from "firebase/auth
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBx1f7lmdlDdqhM8eP5BeDSOwJ0pTVNoaQ",
-authDomain: "blogapplication-d2e4e.firebaseapp.com",
-projectId: "blogapplication-d2e4e",
-storageBucket: "blogapplication-d2e4e.appspot.com",
-messagingSenderId: "11800389790",
-appId: "1:11800389790:web:b40089015d20294b15338f",
-measurementId: "G-LL1PWFVVJV"
 };
 
 // Initialize Firebase
